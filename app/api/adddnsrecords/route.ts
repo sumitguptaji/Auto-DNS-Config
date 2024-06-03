7e7996c4abe6da90ea6a24ebc3cd7be67c5acf37
sumitguptaji <35533649+sumitguptaji@users.noreply.github.com>
2024-06-03 14:34:13 +0530

    Update route.ts

--- app/api/adddnsrecords/route.ts
+++ app/api/adddnsrecords/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
   const dnsRecords = [
     {
         type: 'MX',
-        name: 'yourdomain.com', // Replace with your actual domain name
+        name: '@', // Replace with your actual domain name
         content: 'ASPMX.L.GOOGLE.COM',
         priority: 1,
         ttl: 3600,
@@ -28,7 +28,7 @@ export async function POST(req: Request) {
     },
     {
         type: 'MX',
-        name: 'yourdomain.com', // Replace with your actual domain name
+        name: '@', // Replace with your actual domain name
         content: 'ALT1.ASPMX.L.GOOGLE.COM',
         priority: 5,
         ttl: 3600,
@@ -36,7 +36,7 @@ export async function POST(req: Request) {
     },
     {
         type: 'MX',
-        name: 'yourdomain.com', // Replace with your actual domain name
+        name: '@', // Replace with your actual domain name
         content: 'ALT2.ASPMX.L.GOOGLE.COM',
         priority: 5,
         ttl: 3600,
@@ -44,7 +44,7 @@ export async function POST(req: Request) {
     },
     {
         type: 'MX',
-        name: 'yourdomain.com', // Replace with your actual domain name
+        name: '@', // Replace with your actual domain name
         content: 'ALT3.ASPMX.L.GOOGLE.COM',
         priority: 10,
         ttl: 3600,
@@ -52,7 +52,7 @@ export async function POST(req: Request) {
     },
     {
         type: 'MX',
-        name: 'yourdomain.com', // Replace with your actual domain name
+        name: '@', // Replace with your actual domain name
         content: 'ALT4.ASPMX.L.GOOGLE.COM',
         priority: 10,
         ttl: 3600,
@@ -60,28 +60,28 @@ export async function POST(req: Request) {
     },
     {
         type: 'TXT',
-        name: 'yourdomain.com', // Replace with your actual domain name
+        name: '@', // Replace with your actual domain name
         content: 'v=spf1 include:_spf.google.com ~all',
         ttl: 3600,
         proxied: false
     },
     {
         type: 'TXT',
-        name: '_dmarc.yourdomain.com', // Replace with your actual domain name
+        name: '_dmarc', // Replace with your actual domain name
         content: 'v=DMARC1; p=quarantine; sp=quarantine; adkim=s; aspf=s; rua=mailto:aggregate@seonss.com; ruf=mailto:dmarc@seonss.com; rf=afrf; pct=100; ri=86400',
         ttl: 3600,
         proxied: false
     },
     {
         type: 'TXT',
-        name: 'dkim._domainkey.yourdomain.com', // Replace with your actual domain name
+        name: 'dkim._domainkey', // Replace with your actual domain name
         content: 'v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAozgjXw08gN7I4FcrYCDaJW/TLhw8wlPXlrKaJdF1OeH9jos62g7x8ZSQj85tmFOGYKeqIbiLpwyKBe9tMY2KxftMBuPUFJlZbu93OujQBP6BzC7oBx3+cCHaP3WQ4gXtILGGbwT+MQl2k456F7JYnK2lQNgvPsN7QxPY+ONakcECbIszijxnK1efGIk7/eHDNZnF3eqIQXVvatFXB0aITZdADyZDmJXf1IGYso4XjhoQd7NbD2RhpcWJrIQJxcTDLU05GHchJb+HZ6YaFcT5ZP1V6IAhpnN4WrpJmq/wlff5gBFoGXWFE1RqxpSqLE4eEuxbBehJiILEGyn61qjxmQIDAQAB',
         ttl: 3600,
         proxied: false
     },
     {
         type: 'CNAME',
-        name: 'inst.yourdomain.com', // Replace with your actual domain name
+        name: 'inst', // Replace with your actual domain name
         content: 'prox.itrackly.com',
         ttl: 3600,
         proxied: false
