45c6b4c622b4fec7fc31e5a38385606741bc629c
sumitguptaji <35533649+sumitguptaji@users.noreply.github.com>
2024-06-13 20:56:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -128,7 +128,7 @@ const AddZoneComponent = () => {
 
       {!!serverNames?.length && (
         <React.Fragment>
-          <div className="mb-2">This domain name is {domainName}</div>
+          <div className="mb-2">Nameservers for {domainName}</div>
           <ul className="list-disc pl-5 space-y-2">
             {serverNames.map((v: any) => (
               <li
